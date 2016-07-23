3a2093d2752330fb5ac62ce1cb14345939712991
xiangnan.yang <xiangnan.yang@dianping.com>
2016-07-23 09:40:26 +0800

    Initial Commit

---
+++ headertest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  headertest
+//
+//  Created by yxn on 16/7/23.
+//  Copyright © 2016年 yxn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ headertest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  headertest
+//
+//  Created by yxn on 16/7/23.
+//  Copyright © 2016年 yxn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
